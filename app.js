9691111c777d3e08cb8f292cdc4f37b7be6875e2
Abhishek Jha <Abhishek Jha>
2016-12-03 22:13:00 -0500

    Calculator program

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 (function)(){
-  'use strict';
-var  x ="hello";
-  angular.module('myFirstApp',[])
+'use strict';
 
-  .controller('myFirstController',function(){
+angular.module('NameCalculator',[])
+.controller('NameCalculatorController',function($scope){
+$scope.name ="";
+$scope.totalValue =0;
+$scope.displayNumeric =function(){
+  var totalNameValue =calculatenumericForString($scope.name);
+  $scope.totalValue = totalNameValue;
+};
 
-  });
+function calculatenumericForString(string){
+  var totalStringValue =0;
+  for(var i =0 ;i<string.length;i++){
+    totalStringValue+= string.char(i);
+  }
+  return totalStringValue;
+}
+});
 })();
